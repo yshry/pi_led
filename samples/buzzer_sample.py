951e63a62b1b4b3cb63b3bcf1f5fab22156b1722
yshry <yshry.dev@gmail.com>
2017-07-20 05:29:15 +0000

    updated

--- samples/buzzer_sample.py
+++ samples/buzzer_sample.py
@@ -1,16 +1,14 @@
 import sys
 sys.path.append('../src')
 
-from mcp3208 import Mcp3208
 from buzzer import Buzzer
 from time import sleep
 
-mymcp = Mcp3208(11, 10, 9, 8)
-mybuzzer = Buzzer(18)
+mybuzzer = Buzzer(12)
 
 scale = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
 
-for i in range(1,3):
+for i in range(2,4):
 	for tone in scale:
 		mybuzzer.softtonewrite(tone, i)
 		sleep(1)
