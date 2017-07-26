0436ab941f06a34335d4d69e78281824f53e1342
yshry <yshry.dev@gmail.com>
2017-07-26 11:43:48 +0000

    updated

--- app/beep_back.py
+++ app/beep_back.py
@@ -1,3 +1,4 @@
+import os
 import sys
 sys.path.append('../src')
 
@@ -10,8 +11,14 @@ mybuzzer = Buzzer(12)
 myled = Led(21)
 fname ='./web/beep.json'
 
+currentstat = os.stat(fname).st_mtime
+oldstat = currentstat
+
 try:
 	while True:
+		currentstat = os.stat(fname).st_mtime
+		if oldstat == currentstat:
+			continue
 		try:
 			f=open(fname, 'r')
 			json_dict = json.load(f)
@@ -19,17 +26,18 @@ try:
 			tone = json_dict['tone']
 			level = int(json_dict['level'])
 			if tone != 'na':
-				print json_dict
+				#print json_dict
 				mybuzzer.softtonewrite(tone, level)
 				myled.on()
 			else:
 				mybuzzer.softtonestop()
 				myled.off()
+			oldstat = currentstat
 		except:
 			f.close()
 			print (sys.exc_info())
 			mybuzzer.softtonestop()
 			myled.off()
-		time.sleep(0.25)
+		time.sleep(0.15)
 except KeyboardInterrupt:
 	pass
