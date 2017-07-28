4f4b28244102298e65384df03b8f58366a68223d
yshry <yshry.dev@gmail.com>
2017-07-29 00:58:38 +0900

    update car

--- app/web/car_front.py
+++ app/web/car_front.py
@@ -6,14 +6,11 @@ import sys
 sys.path.append('../../src')
 from pisocket import sendjson
 
-#data = cgi.FieldStorage()
-
-#direction = data.getvalue('direction', 'stop')
-
-json_dict = {'direction': 'back'}
+data = cgi.FieldStorage()
+direction = data.getvalue('direction', 'stop')
 
 print "Content-type: text\n\n"
-
+json_dict = {'direction' : direction}
 complete = (sendjson('localhost', 10080, json_dict) == 1) 
 
 print complete
